bf81d4157799ef9dbbf7177c9542f4fe480fb678
Artur Bajll <artur.bajll@gmail.com>
2022-12-16 22:08:02 +0100

    pep8 fix

--- day1/day1.py
+++ day1/day1.py
@@ -1,4 +1,4 @@
-#Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
+# Go to: https://replit.com/@appbrewery/band-name-generator-start?v=1
 print("Welcome to the Band Name Generator.")
 city = input("What's name of the city you grew up in?\n")
 pet = input("What's your pet's name?\n")
